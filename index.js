87f0e44c08f708290cc1737ebe09b016525d8077
João Carmassi <joaovitorcarmassi@gmail.com>
2024-09-17 01:32:28 -0300

    primerio commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// Vars -----
+const alphabet =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz0123456789!?.,;: àáâãäåèéêëìíîïðòóôõö÷øùúûüýÿÀÁÂÃÄÅÈÉÊËÌÍÎÏÐÒÓÔÕÖ×ØÙÚÛÜÝ@#$%^&*()-_=+[]{}|\\`~\"'<>\n\r\t";
+const listaDeLetras = alphabet.split("");
+let criandoFrase = [];
+let x = 0;
+let y = 0;
+const titulo = document.querySelector("h1");
+const span = document.querySelector("div>span");
+const btn = document.querySelector("button");
+let frase;
+let fraseSeparada;
+let velocidade;
+let intervalo;
+
+// Código -----
+btn.addEventListener("click", () => {
+  frase = document.getElementById("inputFrase").value;
+  fraseSeparada = frase.split("");
+  velocidade = document.getElementById("inputIntervalo").value;
+  span.innerHTML = "";
+  intervalo = setInterval(montaFrase, velocidade);
+});
+
+function montaFrase() {
+  if (criandoFrase.length !== fraseSeparada.length) {
+    let gerandoFrase = `${criandoFrase.join("")}${listaDeLetras[y]}`;
+    exibeFrase(gerandoFrase);
+
+    if (listaDeLetras[y] === fraseSeparada[x]) {
+      criandoFrase.push(listaDeLetras[y]);
+      x++;
+      y = 0;
+    } else {
+      y++;
+    }
+  } else {
+    clearInterval(intervalo);
+  }
+}
+
+function exibeFrase(fraseNova) {
+  if (span.firstChild) {
+    const p = document.createElement("p");
+    p.textContent = fraseNova;
+    span.appendChild(p);
+    p.scrollIntoView();
+  } else {
+    document.querySelector("div").style.backgroundColor = "black";
+    span.classList.add("painelPreto");
+    span.innerHTML = `
+    <p>Microsoft Windows [versão 10.0.22631.4169]</p>
+    <p>(c) Microsoft Corporation. Todos os direitos reservados.</p>
+    `;
+  }
+}
